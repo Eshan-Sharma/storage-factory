97e70cb62b430b6023672f9e02c3411eef092021
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-08 10:52:02 +0530

    array of simple storage contracts and ABI

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,10 +4,24 @@ pragma solidity ^0.8.18;
 import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     //function to deploy a contract from a contract
     function createSimpleStorageContact() public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex,uint256 _simpleStorageNumber) public{
+        //To interact with a contract you need
+        //address
+        //ABI - application binary interface
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 }
